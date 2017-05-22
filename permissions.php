53ea4d96a879abcd956557165e8c20f38404bb86
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-22 16:57:58 -0300

    Criação das permissões básicas.

---
+++ permissions.php
@@ -0,0 +1,16 @@
+<?php
+add_action('init', function() {
+    if (get_role( 'cadastrador_editais' )) {
+        remove_role( 'cadastrador_editais' );
+    }
+    add_role('cadastrador_cursos', __('Cadastrador de Editais'), array(
+        'read'                   => true,
+        'upload_files'           => true,
+
+        'create_editais'         => true,
+        'edit_editais'           => true,
+        'manage_editais'         => false,
+
+        'assign_edital_category' => true
+    ));
+});
