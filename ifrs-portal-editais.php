91d9c6f3b1a0961527998369b45e593ad29c2d4d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-22 16:57:53 -0300

    Renomeação do plugin para mais semântica.

--- ifrs-portal-editais.php
+++ ifrs-portal-editais.php
@@ -1,7 +1,7 @@
 <?php
 defined('ABSPATH') or die('No script kiddies please!');
 /*
-Plugin Name: IFRS Portal Plugin Editais
+Plugin Name: IFRS Portal Editais
 Plugin URI:  https://github.com/IFRS/portal-plugin-editais
 Description: Plugin para gerenciar Editais.
 Version:     1.0.0
@@ -15,3 +15,4 @@ Domain Path: /lang
 
 require_once('edital-category.php');
 require_once('edital.php');
+require_once('permissions.php');
