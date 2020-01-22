32d410fd12b2ec7a76754cafde490911ac371534
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-01-22 08:47:31 -0300

    Remoção de marcação desnecessária

--- widgets/latest.php
+++ widgets/latest.php
@@ -23,41 +23,33 @@ class Editais_Widget extends WP_Widget {
 
         if ($latest_editais->have_posts()) :
 ?>
-            <div class="row">
-                <div class="col-12">
-                    <div class="ultimos-editais">
-                        <?php
-                            if ( ! empty( $instance['title'] ) ) {
-                                echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
-                            }
-                        ?>
-                        <?php while ($latest_editais->have_posts()) : $latest_editais->the_post(); ?>
-                            <div class="row ultimos-editais__edital">
-                                <div class="col-12">
-                                    <p class="ultimos-editais__edital-datetime">
-                                        <?php echo get_the_modified_date('d/m/Y'); ?>
-                                        &agrave;s
-                                        <?php echo get_the_modified_time('G\hi'); ?>
-                                    </p>
-                                    &bull;
-                                    <?php echo get_the_term_list(get_the_ID(), 'edital_category', '<ul class="ultimos-editais__edital-categories"><li>', ',&nbsp;</li><li>', '</li></ul>'); ?>
-                                    <h3 class="ultimos-editais__edital-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                </div>
-                            </div>
-                        <?php endwhile; ?>
+            <div class="ultimos-editais">
+                <?php
+                    if ( ! empty( $instance['title'] ) ) {
+                        echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+                    }
+                ?>
+                <?php while ($latest_editais->have_posts()) : $latest_editais->the_post(); ?>
+                    <div class="row ultimos-editais__edital">
+                        <div class="col-12">
+                            <p class="ultimos-editais__edital-datetime">
+                                <?php echo get_the_modified_date('d/m/Y'); ?>
+                                &agrave;s
+                                <?php echo get_the_modified_time('G\hi'); ?>
+                            </p>
+                            &bull;
+                            <?php echo get_the_term_list(get_the_ID(), 'edital_category', '<ul class="ultimos-editais__edital-categories"><li>', ',&nbsp;</li><li>', '</li></ul>'); ?>
+                            <h3 class="ultimos-editais__edital-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        </div>
                     </div>
-                </div>
+                <?php endwhile; ?>
             </div>
 
             <?php wp_reset_query(); ?>
 
-            <div class="row">
-                <div class="col-12">
-                    <div class="acesso-todos-editais">
-                        <hr class="acesso-todos-editais__separador">
-                        <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="float-right acesso-todos-editais__link"><?php _e('Acesse todos os Editais'); ?></a>
-                    </div>
-                </div>
+            <div class="acesso-todos-editais">
+                <hr class="acesso-todos-editais__separador">
+                <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="float-right acesso-todos-editais__link"><?php _e('Acesse todos os Editais'); ?></a>
             </div>
 <?php
         endif;
